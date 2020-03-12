819cbff7b37cf135519ac31b449af690edf5fbfc
apple <apple@apples-MacBook-Pro-2.local>
2020-03-12 13:57:51 -0700

    setting up endpoints

--- users/userRouter.js
+++ users/userRouter.js
@@ -33,6 +33,15 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  Users.getById(req.params.id)
+  .then(user => {
+    if(user){
+      res.status(200).json(user)
+    } else {
+      res.status(400).json({error: "Invalid user id"})
+    }      
+  })
+  .catch(err => res.status(500).json({error: "Error fetching user"}))
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -50,16 +59,58 @@ router.get('/:id/posts', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  Users.remove(req.params.id)
+  .then(user => {
+    if(user > 0){
+      res.status(200).json({message: "User deleted"})
+    } else {
+      res.status(400).json({error: "User could not be deleted"})
+    }
+  })
+  .catch(err => res.status(500).json({error: "Error deleting user"}))
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  const id = req.params.id;
+  const { name } = req.body;
+
+  if(!name){
+    res.status(400).json({error: "Name field is required"})
+  }
+
+  Users.update(id, req.body)
+  .then(user => {
+    if(user){
+      res.status(200).json({message: "User updated successfully"});
+    } else {
+      res.status(404).json({error: "No user with that id exists"})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({error: "Error updating user"})
+    console.log(err)
+  })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!s
+  const {id} = req.params;
+
+  Users.getById(id)
+  .then(userId => {
+    if(userId){
+      userId = req.user;
+      next();
+    }else{
+      res.status(400).json({error: "Invalid user id"})
+    }
+  })
+  .catch (err =>{
+    console.log(res.status(500).json({error: "There was an error validating the user id", err}))
+  })
 }
 
 function validateUser(req, res, next) {
